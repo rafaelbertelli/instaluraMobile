4093440c9f31bdadbd268ce9432d91bec3d6ea7f
Rafael <rafaelbertelli@gmail.com>
2018-07-02 18:32:32 -0300

    created app like instagram

--- App.js
+++ App.js
@@ -8,17 +8,11 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
-  ScrollView,
-  View,
-  Image,
-  Dimensions,
   FlatList,
+  View,
 } from 'react-native';
 
-import Goku from './resources/img/goku.jpg';
-
-const _width = Dimensions.get('screen').width;
+import Post from './src/components/post'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -29,29 +23,33 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor () {
+    super()
+
+    this.state = {
+      fotos: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+      .then(res => res.json())
+      .then(json => this.setState({ fotos: json }))
+      .catch(err => console.err(err))
+  }
+
   render () {
-
-    const box = [
-      { id: '1', 'name': 'Hécate' },
-      { id: '2', 'name': 'Cátia' },
-      { id: '3', 'name': 'Elimba' },
-    ]
-
     return (
-      <FlatList
-      data={box}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => (
-        <View style={styles.container}>
-          <View style={styles.userPresentation}>
-            <Image source={Goku} style={styles.profilePhoto} />
-            <Text style={styles.welcome}>{item.name}</Text>
-          </View>
-          <Image source={Goku} style={{ width: _width, height: _width }} />
-        </View>
-      )}
-      />
-    );
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.fotos}
+          keyExtractor={item => item.id.toString()}
+          renderItem={ ({ item }) => (
+            <Post foto={item} />
+          )}
+        />
+      </View>
+    )
   }
 }
 
@@ -60,25 +58,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  userPresentation: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    margin:10,
-  },
-  profilePhoto: {
-    width: 40,
-    height: 40,
-    borderRadius: 20
   }
-});
+})
